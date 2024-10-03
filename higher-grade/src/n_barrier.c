3e688b49d555d4fc99ff415f87a92ad6da3bf65e
kamar535 <karl.marklund@gmail.com>
2024-10-03 15:48:25 +0200

    Removed psem_test from mandatory/Makefile

--- higher-grade/src/n_barrier.c
+++ higher-grade/src/n_barrier.c
@@ -8,42 +8,42 @@
  *
  */
 
-#include <stdio.h>      // perror()
-#include <stdlib.h>     // exit(), EXIT_FAILURE
+#include "n_barrier.h"  // The barrier API
 
-#include "n_barrier.h"   // The barrier API
- 
+#include <stdio.h>   // perror()
+#include <stdlib.h>  // exit(), EXIT_FAILURE
+
 void barrier_init(barrier_t *barrier, int count) {
-    if (pthread_mutex_init(&barrier->mutex, NULL) < 0) {
-        perror("Init mutex lock");
-        exit(EXIT_FAILURE);
-    }
+  if (pthread_mutex_init(&barrier->mutex, NULL) < 0) {
+    perror("Init mutex lock");
+    exit(EXIT_FAILURE);
+  }
 
-    barrier->count      = count;
-    barrier->arrived    = 0;
-    barrier->semaphore  = psem_init(0);
+  barrier->count = count;
+  barrier->arrived = 0;
+  barrier->semaphore = psem_init(0);
 }
 
 void barrier_destroy(barrier_t *barrier) {
-       psem_destroy(barrier->semaphore);
-       pthread_mutex_destroy(&barrier->mutex);
+  psem_destroy(barrier->semaphore);
+  pthread_mutex_destroy(&barrier->mutex);
 }
 
 void barrier_wait(barrier_t *barrier) {
-    pthread_mutex_lock(&barrier->mutex);
-    
-    barrier->arrived++;
-    
-    if (barrier->arrived  == barrier->count) {
-        for (unsigned int i = 0; i < barrier->count - 1; i ++) {
-            psem_signal(barrier->semaphore);
-        }
+  pthread_mutex_lock(&barrier->mutex);
 
-        barrier->arrived = 0;
+  barrier->arrived++;
 
-        pthread_mutex_unlock(&barrier->mutex);
-    } else {
-        pthread_mutex_unlock(&barrier->mutex);
-        psem_wait(barrier->semaphore);
+  if (barrier->arrived == barrier->count) {
+    for (unsigned int i = 0; i < barrier->count - 1; i++) {
+      psem_signal(barrier->semaphore);
     }
+
+    barrier->arrived = 0;
+
+    pthread_mutex_unlock(&barrier->mutex);
+  } else {
+    pthread_mutex_unlock(&barrier->mutex);
+    psem_wait(barrier->semaphore);
+  }
 }
